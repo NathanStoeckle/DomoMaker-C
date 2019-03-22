c1110466e9141a1210c1cf8eefcbdd7d0cfc3b48
NathanStoeckle <31453002+NathanStoeckle@users.noreply.github.com>
2019-03-22 14:08:45 -0400

    Added a fix (hopefully)

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,7 +2,7 @@ const requiresLogin = (req, res, next) => {
   if (!req.session.account) {
     return res.redirect('/');
   }
-  
+
   return next();
 };
 
@@ -10,7 +10,7 @@ const requiresLogout = (req, res, next) => {
   if (req.session.account) {
     return res.redirect('/maker');
   }
-  
+
   return next();
 };
 
@@ -18,8 +18,8 @@ const requiresSecure = (req, res, next) => {
   if (req.headers['x-forwarded-proto'] !== 'https') {
     return res.reditect(`https://${req.hostname}${req.url}`);
   }
-  
-  return next;
+
+  return next();
 };
 
 const bypassSecure = (req, res, next) => {
